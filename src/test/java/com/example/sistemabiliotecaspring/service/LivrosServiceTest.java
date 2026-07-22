14c55df5035999cc680ed197bdc721d89175b079
bieleiji <146545505+bieleiji@users.noreply.github.com>
2026-07-22 20:31:24 -0300

    implementação de todos os testes unitários para o método atualizarLivro() de LivrosService

--- src/test/java/com/example/sistemabiliotecaspring/service/LivrosServiceTest.java
+++ src/test/java/com/example/sistemabiliotecaspring/service/LivrosServiceTest.java
@@ -2,6 +2,7 @@ package com.example.sistemabiliotecaspring.service;
 
 import com.example.sistemabiliotecaspring.dto.livroDTO.LivroRequest;
 import com.example.sistemabiliotecaspring.exception.RecursoNaoAutorizadoException;
+import com.example.sistemabiliotecaspring.exception.RecursoNaoEncontradoException;
 import com.example.sistemabiliotecaspring.model.Livro;
 import com.example.sistemabiliotecaspring.repository.LivrosRepository;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,10 +28,14 @@ public class LivrosServiceTest {
     @InjectMocks
     private LivrosService livrosService;
 
+    private final int PAGE = 0;
+    private final int SIZE = 10;
+
+    private final long ID = 1L;
+
+    private final String TITULO = "Senhor dos Aneis";
 
     // getLivros()
-    private final int PAGE = 0, SIZE = 10;
-    private final String TITULO = "Senhor dos Aneis";
 
     @Test
     public void getLivrosTestSemFiltro() {
@@ -106,5 +111,47 @@ public class LivrosServiceTest {
         livrosService.salvarLivro(livroRequest, authentication);
 
         verify(livrosRepository).save(new Livro(livroRequest.getNome()));
+    }
+
+
+
+    // atualizarLivro()
+
+    @Test
+    public void atualizarLivroTestUsuarioNaoEhAdmin() {
+        Authentication authentication = mock(Authentication.class);
+        LivroRequest livroRequest = new LivroRequest();
+
+        when(authentication.getAuthorities()).thenReturn(List.of());
+
+        assertThrows(RecursoNaoAutorizadoException.class,
+                () -> livrosService.atualizarLivro(ID, livroRequest, authentication));
+
+    }
+
+    @Test
+    public void atualizarLivroTestLivroNaoEncontrado() {
+        Authentication authentication = mock(Authentication.class);
+        LivroRequest livroRequest = new LivroRequest();
+
+        when(authentication.getAuthorities()).thenAnswer(invocation -> List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        when(livrosRepository.findById(ID)).thenReturn(Optional.empty());
+
+        assertThrows(RecursoNaoEncontradoException.class,
+                () -> livrosService.atualizarLivro(ID, livroRequest, authentication));
+
+    }
+
+    @Test
+    public void atualizarLivroSucesso() {
+        Authentication authentication = mock(Authentication.class);
+        LivroRequest livroRequest = new LivroRequest(TITULO);
+
+        when(authentication.getAuthorities()).thenAnswer(invocation -> List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        when(livrosRepository.findById(ID)).thenReturn(Optional.of(new Livro(ID, "Nome Antigo", false)));
+
+        livrosService.atualizarLivro(ID, livroRequest, authentication);
+
+        verify(livrosRepository).save(new Livro(ID, TITULO, false));
     }
 }
